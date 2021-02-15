a7bfd56dcc749e35ff1564b64b2c55aec48e6a9f
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-15 16:29:13 -0700

    Added gitignore and scanner

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static Map<Integer, String> phonebook = new HashMap<>();
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
