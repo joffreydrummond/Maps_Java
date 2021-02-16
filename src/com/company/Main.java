b2fb2c1a6d1cb4d56076b02fa9eb4ff799ab5d70
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-15 18:41:18 -0700

    added printmenu and userchoice methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,45 @@ public class Main {
     static Map<Integer, String> phonebook = new HashMap<>();
 
     public static void main(String[] args) {
-	// write your code here
+        int choice = 0;
+
+        while (choice != 4) {
+            printMenu();
+            choice = getUserChoice();
+
+            if (choice == 1) {
+                displayPhonebook();
+//                else if (choice == 2) {
+////                    addContact();
+//                    else if (choice == 3) {
+////                        deleteContact();
+//                        else if (choice == 4) {
+//                            System.out.println("See you next time. Goodbye!");
+//                        } else {
+//                            System.out.println("Please select a valid option.");
+//                        }
+//                    }
+//                }
+            }
+        }
+        // write your code here
     }
+
+    public static void printMenu(){
+        System.out.println("1) Display Phonebook");
+        System.out.println("2) Add New Contact(s)");
+        System.out.println("3) Delete Contact(s)");
+        System.out.println("4) Exit Phonebook");
+        System.out.println("########################################");
+    }
+
+    public static int getUserChoice(){
+        return scanner.nextInt();
+    }
+
+    public static void displayPhonebook(){
+
+    }
+
+
 }
